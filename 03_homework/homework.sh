825a59c70962b496c8bd0918e0cef25b484ff7d8
Dmytro-Bonislavskyi <133176607+Dmytro-Bonislavskyi@users.noreply.github.com>
2024-04-23 23:23:51 -0400

    Update homework.sh with spaces between tasks

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -24,12 +24,14 @@ done
 for dir in dir1 dir2 dir3 dir4 dir5; do
     ls "$dir"
 done
+
 # 5. How would you append to one of the existing files " and machine learning!"?
 echo " and machine learning!" >> dir5/file5.txt
+
 # 6. How would you verify that the text was indeed appended to the existing file?
 cat dir5/file5.txt
-# 7. How would you delete all files except for the one with the appended text?
 
+# 7. How would you delete all files except for the one with the appended text?
 dirToKeepFile="dir5"
 fileToKeep="file5.txt"
 for dir in dir1 dir2 dir3 dir4 dir5; do
@@ -38,7 +40,7 @@ for dir in dir1 dir2 dir3 dir4 dir5; do
         if [ "file$i.txt" == "$fileToKeep" ] && [ "$dir" == "$dirToKeepFile" ]; then
             echo "I will not delete $dir/file$i.txt"
         else
-		    rm "file$i.txt"
+	    rm "file$i.txt"
             echo "delete $dir/file$i.txt"
         fi
     done
@@ -48,7 +50,9 @@ cd dir5
 
 # 8. How would you navigate back to the parent directory containing all the directories?
 cd ..
+
 # 9. How would you remove each directory along with its contents?
 rm -r dir1 dir2 dir3 dir4 dir5
+
 # 10. How would you verify that all directories and files have been deleted?
 ls -la
