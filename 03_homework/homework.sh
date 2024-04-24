0441e7a19a4bce70a633ae348239e5bcbd4e385c
Dmytro-Bonislavskyi <133176607+Dmytro-Bonislavskyi@users.noreply.github.com>
2024-04-23 23:10:10 -0400

    Updated homework.sh with answers

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -29,9 +29,9 @@ echo " and machine learning!" >> dir5/file5.txt
 # 6. How would you verify that the text was indeed appended to the existing file?
 cat dir5/file5.txt
 # 7. How would you delete all files except for the one with the appended text?
+
 dirToKeepFile="dir5"
 fileToKeep="file5.txt"
-
 for dir in dir1 dir2 dir3 dir4 dir5; do
     cd "$dir"
     for i in {1..5}; do
@@ -40,12 +40,12 @@ for dir in dir1 dir2 dir3 dir4 dir5; do
         else
 		    rm "file$i.txt"
             echo "delete $dir/file$i.txt"
-
         fi
     done
     cd ..
 done
 cd dir5
+
 # 8. How would you navigate back to the parent directory containing all the directories?
 cd ..
 # 9. How would you remove each directory along with its contents?
